cffb5ef465755dcefea97f01270ec033a1c07c04
Demon Martin <49092993+DemonMartin@users.noreply.github.com>
2022-02-07 18:18:43 +0100

    update

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-// Made with ❤️ by Demon Martin#0193!
+// Made with ❤️ by Demon Martin#0193! Btw. I am not a good coder at all, so please correct and report any bugs found at https://github.com/Dojnaz/PteroTools/issues
+
+//PACKAGES
 const { prompt } = require('enquirer');
+const fs = require("fs")
+//JSON
+if(!fs.readFileSync(`./config.json`)) return
+let config = JSON.parse(fs.readFileSync(`./config.json`))
+
+console.log(config)
